6e890a07e7e8472eabbb084ee5463597e9bfeead
farhan2703 <farhanminions.20@gmail.com>
2022-10-06 05:33:41 +0700

    #Membuat input username & password, dan button login

---
+++ lib/landing/landing_page.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<style>
+    body{background: linear-gradient(45deg, #D6D4D4, #90C5D6) !important;}
+    .pt{
+        padding-top: 10rem;
+    }
+    .bt{
+         padding-top: 1rem;
+    }
+</style>
+    <title>Document</title>
+</head>
+<body>
+<div class="container p-5">
+  <div class="card" style="width: 18rem; height: 27rem;">
+    <div class="card-body pt">
+    <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+  <div class="d-grid bt gap-2 col-10 mx-auto">
+  <button class="btn btn-primary" type="button">Login </button>
+  <button class="btn btn-success" type="button">Login</button>
+</div>
+  </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
