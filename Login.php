2f563b5be87d9b8b95904319a7790b328d76ec0b
Lynaaprilia <lynaaprilia27@gmail.com>
2022-10-06 14:10:28 +0700

    login

---
+++ Login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+
+                <div class="text-center">
+                  <img src="assets/logo.png"
+                    style="width: 185px;" alt="logo">
+                  <h4 class="mt-1 mb-5 pb-1">Ijen Batik Bondowoso</h4>
+                </div>
+
+                <form>
+
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example11">Username</label>
+                    <input type="email" id="form2Example11" class="form-control"
+                      placeholder="Username" />
+                   
+                  </div>
+
+                  <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example22">Password</label>
+                    <input type="password" id="form2Example22" class="form-control" 
+                    placeholder="Password"/>
+                    
+                  </div>
+
+                  <div class="text-center pt-1 mb-2 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+                      in</button>
+                   
+                  </div>
+                  <div class="text-center pt-1 mb-5 pb-2">
+                    <a class="text-muted" href="#!">Forgot password?</a>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+              <div class="text-black px-3 py-4 p-md-5 mx-md-4">
+              <div class="text-center">
+                  <!-- <img src="assets/logo2.png"
+                    style="width: 185px;" alt="logo"> -->
+                  <h4 class="mt-1 mb-3 pb-1">Ijen Batik Bondowoso</h4>
+                </div>
+                <p class="small mb-0">Kami Ijen Batik Bondowoso membuat Batik Tulis Batik Cap dan Abstrack,
+desain dan warna beda dengan yang lain, kami membuat warna dengan cara gradasi atau sembur dengan tekhnik batik lukis,
+adapun motif kami yaitu Motif Singkong.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
+
+
