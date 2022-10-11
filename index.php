0573e51fb8de29fc52c16e51fe40bda91a6c64f6
GilangNugraha32 <100219604+GilangNugraha32@users.noreply.github.com>
2022-10-11 19:57:33 +0700

    design baru login

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-<section class="h-100 gradient-form" style="linear-gradient(45deg, #D6D4D4, #90C5D6) !important; ">
-  <div class="container py-5 h-100">
+ 
+<section class="h-100 gradient-form">
+
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-xl-10">
-        <div class="card rounded-3 text-black">
+
+      
+
+        <div class="container">
           <div class="row g-0">
             <div class="col-lg-6">
               <div class="card-body p-md-5 mx-md-4">
@@ -27,26 +31,33 @@
                   <label class="form-label" for="form2Example11">Username</label>
                     <input type="email" id="form2Example11" class="form-control"
                       placeholder="Username" />
-                   
                   </div>
 
                   <div class="form-outline mb-4">
                   <label class="form-label" for="form2Example22">Password</label>
                     <input type="password" id="form2Example22" class="form-control" 
                     placeholder="Password"/>
-                    
                   </div>
 
-                  <div class="text-center pt-1 mb-2 pb-1">
-                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                      in</button>
-                   
-                  </div>
-                  <div class="text-center pt-1 mb-5 pb-2">
+                  <div class="f2orm-footer mt-2">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Remember me 
+                      </label>
+                      <div class="text-allign-bottom pt-1 mb-5 pb-2">
                     <a class="text-muted" href="#!">Forgot password?</a>
                   </div>
-                  <div class="form-footer mt-2">
-                    <p> Sudah punya account? <a href="register.php">REGISTER</a></p>
+                  </div>
+                        
+                  <div class="d-grid gap-2">
+                    <button class="p-2 btn btn-success" type="button">LOGIN SUPER ADMIN</button>
+                    <button class="p-2 btn btn-primary" type="button">LOGIN ADMIN</button>
+                        </div>
+                  </div>
+
+                  <div class="f2orm-footer mt-2">
+                    <p>  Belum punya akun? <a href="register.php">REGISTER</a></p>
                 </div>
 
                 </form>
@@ -61,16 +72,21 @@
                   <h4 class="mt-1 mb-3 pb-1">Ijen Batik Bondowoso</h4>
                 </div>
                 <p class="small mb-0">Kami Ijen Batik Bondowoso membuat Batik Tulis Batik Cap dan Abstrack,
-desain dan warna beda dengan yang lain, kami membuat warna dengan cara gradasi atau sembur dengan tekhnik batik lukis,
-adapun motif kami yaitu Motif Singkong.</p>
+                                      desain dan warna beda dengan yang lain, kami membuat warna dengan cara gradasi atau sembur dengan tekhnik batik lukis,
+                                      adapun motif kami yaitu Motif Singkong.</p>
               </div>
             </div>
           </div>
         </div>
+        </div>
+      </div>
       </div>
     </div>
   </div>
 </section>
+
+
+
 </body>
 </html>
 
