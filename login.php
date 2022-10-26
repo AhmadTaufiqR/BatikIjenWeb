6f494d4ee62ab19f1b43f4d59f840d25bc9bd412
farhan2703 <farhanminions.20@gmail.com>
2022-10-26 10:09:53 +0700

    Membuat tampilan register

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">   
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>SANGGAR BATIK</title>
+    <!-- menyisipkan bootstrap -->
+    </a>
+  </div>
+</nav>
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Selamat datang di Sanggar Batik </h1>
+                        <p>Silahkan Login..</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10">
+                    <img class="asset"  />
+                </div>
+            </div>
+        </div>
+    <tr> 
+        <br>
+        <td>Username</td>
+        <td><input type="text" name="username"></d>
+</br>
+    </tr>
+<tr>
+    <br>
+    <td>password</td>
+    <td><input type="password" name="password"></td>
+</br>
+</tr>
+
+
+    <br>
+        <a href="login.php" class="btn btn-secondary">Masuk</a>
+        <a href="register.php" class="btn btn-success">Daftar</a>
+                    </div>
+</br>
+<div class="h4 pb-2 mb-4 text-danger ">
+</div>
+</body>
+</html>
