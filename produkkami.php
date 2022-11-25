5946050638f5ff84927d3cbc0c375c80ef46c7ba
Lynaaprilia <lynaaprilia27@gmail.com>
2022-11-25 21:41:51 +0700

    slide bar

--- produkkami.php
+++ produkkami.php
@@ -1,3 +1,7 @@
+<?php
+include 'koneksi.php';
+$query = mysqli_query($koneksi, "SELECT * from tb_produk ORDER BY tb_produk.nama_produk DESC  ")
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,13 +11,10 @@
 	<!-- Boxicons -->
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<!-- My CSS -->
-	<link rel="stylesheet" href="styledashboard.css">
-
-	<title>Admin</title>
+	<link rel="stylesheet" href="styleprodukkami.css">
+	<title>SanggarBatik</title>
 </head>
 <body>
-
-
 	<!-- SIDEBAR -->
 	<section id="sidebar">
 		<a href="#" class="brand">
@@ -55,8 +56,6 @@
 			</li>
 		</ul>
 		<ul class="side-menu">
-			
-			</li>
 			<li>
 				<a href="logout.php" class="logout">
 					<i class='bx bxs-log-out-circle' ></i>
@@ -67,5 +66,50 @@
 		</ul>
 	</section>
 	<!-- SIDEBAR -->
-
-    
+    	
+	<!-- <div class="container"> -->
+  <!-- Table Produk -->
+</div class="content">
+</div class="container-fluid">
+<div class="row">
+	<div class="col-md-12">
+		<div class="card">
+			<div class="header">
+				<h4 class="title">SanggarBatik</h4>
+				<p class="category">Data Batik</p>
+</div>
+<div class="content table-responsive table-full-width">
+<table class="table table">
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                    </div>
+                                                    <tbody>
+                                                                    <form  class="table" method="post"  style="margin-left:50px; float:left; padding: 80px 150px;" border="1" > </form>
+                                                                    <table class="table table-striped" style="float:left; margin-left: 0px; margin: 0px;" border="1" align="left">
+                                                                        <th class="text-center">Id Produk</th>
+                                                                        <th class="text-center">Nama Produk</th>
+                                                                        <th class="text-center">Stok</th>
+                                                                        <th class="text-center">Harga Produk</th>
+                                                                        <th class="text-center">Jenis Produk</th>
+                                                                        <th class="text-center">Gambar Produk</th>
+                                                                        <th class="text-center">Rincian Produk</th>
+                                                                        <th class="text-center">Ukuran Produk</th>
+                                                                    </thead>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <?php if(mysqli_num_rows($query)) {?>
+                                                                        <?php while($row = mysqli_fetch_array($query)) {?>
+                                                                    <tr>
+                                                                        <td class="text-center"><?php echo $row['id_produk'] ?></td>
+                                                                        <td class="text-center"><?php echo $row['nama_produk'] ?></td>
+                                                                        <td class="text-center"><?php echo $row['stok'] ?></td>
+                                                                        <td class=" text-center"><?php echo $row['harga_produk'] ?></td>
+                                                                        <td class="text-center"><?php echo $row['jenis_produk'] ?></td>
+                                                                        <td class="text-center"><?php echo $row['gambar_produk'] ?></td>
+                                                                        <td class="text-center"><?php echo $row['rincian_produk'] ?></td>
+                                                                        <td class="text-center"><?php echo $row['ukuran_produk'] ?></td>
+                                                                     </tr>
+                                                                     <?php } ?>
+                                                                     <?php } ?>
+                          </tbody>
+                        </table>
--- register.php
+++ register.php
@@ -4,14 +4,15 @@ $username = $_POST["username"];
 $nama_lengkap = $_POST["nama_lengkap"];
 $email = $_POST["email"];
 $password = $_POST["password"];
-$no_telepon = $_POST["no_telepon"];
+$no_telephone = $_POST["no_telephone"];
+$level = $_POST["admin"];
 
 
-$query_sql = "INSERT INTO tb_pengguna (username, nama_lengkap, email, password, no_telepon, level )
-VALUES ('$username', '$nama_lengkap', '$email', '$password', '$no_telepon', 'Admin')";
+$query_sql = "INSERT INTO tb_pengguna (username, nama_lengkap, email, password, no_telephone, level )
+VALUES ('$username', '$nama_lengkap', '$email', '$password', '$no_telephone', 'admin')";
 
 if (mysqli_query($koneksi, $query_sql)) {
     header("Location: index.php");
 }else {
-    echo "Pendaftaran Gagal : " .mysqli_error($conn);
+    echo "Pendaftaran Gagal : " .mysqli_error($koneksi);
 }
